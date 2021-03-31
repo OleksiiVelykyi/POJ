3949d017a1b24097725f5ba734f684291f5e1b21
OleksiiVelykyi <80325994+OleksiiVelykyi@users.noreply.github.com>
2021-03-31 23:02:41 +0200

    Update 3.1.java

--- Lab3/3.1.java
+++ Lab3/3.1.java
@@ -1,1 +1,19 @@
-
+package POJ;
+public class Kwadrat {
+    public static void main(String[] args) {
+        Square square = new Square(2);
+        System.out.printf("Area: %s, perimeter: %s%n", square.GetArea(), square.GetPerimeter());
+    }
+    private static class Square {
+        private final int width;
+        public Square(int width) {
+            this.width = width;
+        }
+        public int GetArea() {
+            return this.width * this.width;
+        }
+        public int GetPerimeter() {
+            return 4 * this.width;
+        }
+    }
+}
