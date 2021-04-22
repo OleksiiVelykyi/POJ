1b8ce5d490e3616bdf25c95759b0882cc9d75fa9
OleksiiVelykyi <80325994+OleksiiVelykyi@users.noreply.github.com>
2021-04-22 13:38:20 +0200

    Create Main.java

---
+++ Lab 6/Main.java
@@ -0,0 +1,10 @@
+package poj.lab6.zad2;
+import java.util.ArrayList;
+public class Main {
+    public static void main(final String[] args) {
+        final Zamowienie zamowienie = new Zamowienie(new ArrayList<>());
+        order.addPosition(new Position("sernik", 2, 2));
+        order.addPosition(new Position("pierogi", 1, 6));
+        System.out.printf("Your order: %s", zamowienie);
+    }
+}
