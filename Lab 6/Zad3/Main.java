8fb1c67b2f45a891f59223185cb5eecc472912e4
OleksiiVelykyi <80325994+OleksiiVelykyi@users.noreply.github.com>
2021-04-22 13:45:43 +0200

    Create Main.java

---
+++ Lab 6/Zad3/Main.java
@@ -0,0 +1,7 @@
+package poj.lab6.zad3;
+public class Main {
+    public static void main(final String[] args) {
+        final MojaData mojaData = new MojaData(1, 11, 2011);
+        System.out.printf("Moja data: %s", mojaData);
+    }
+}
