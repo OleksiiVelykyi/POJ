6363e41c2a1d3ce82f128b1623369cfd8eefed9d
OleksiiVelykyi <80325994+OleksiiVelykyi@users.noreply.github.com>
2021-04-22 13:09:23 +0200

    Update Pozycja.java

--- Lab 6/Zad2/Pozycja.java
+++ Lab 6/Zad2/Pozycja.java
@@ -1,9 +1,9 @@
 package poj.lab6.zad2;
-public class Pozycja {
+public class Position {
     private final String nazwaTowaru;
     private final int ilosc;
     private final double cena;
-    public Pozycja(final String nazwaTowaru, final int ilosc, final double cena) {
+    public Position(final String nazwaTowaru, final int ilosc, final double cena) {
         this.nazwaTowaru = nazwaTowaru;
         this.ilosc = ilosc;
         this.cena = cena;
@@ -13,6 +13,6 @@ public class Pozycja {
     }
     @Override
     public String toString() {
-        return "Pozycja: %s, (%d) %.2f per item, total: %.2f".formatted(this.nazwaTowaru, this.ilosc, this.cena, this.iloscWartosc());
+        return "Position: %s, (%d) %.2f per item, total: %.2f".formatted(this.nazwaTowaru, this.ilosc, this.cena, this.iloscWartosc());
     }
 }
