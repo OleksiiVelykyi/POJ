7482d6302e359e681635fe14f65d6845afb7ad48
OleksiiVelykyi <80325994+OleksiiVelykyi@users.noreply.github.com>
2021-04-22 12:42:23 +0200

    Update Main.java

--- Lab 6/Zad1/Main.java
+++ Lab 6/Zad1/Main.java
@@ -1,19 +1,18 @@
-package poj.lab6
+package poj.lab6;
 
 public class Main {
     public static void main(final String[] args) {
         final Firma firma = new Firma(new Pracownik[] {
-           new Pracownik("Paulina", "Gełbotowska", 60),
+           new Pracownik("Paulina", "Gełbotowska", 80),
            new Pracownik("Michał", "Dubowik", 60),
-           new Pracownik("Regina", "Turska", 120),
-           new Pracownik("Alex", "Derski", 80),
-           new Pracownik("Mikołaj", "Żukowski",80),
-           new Pracownik("Krzysztof", "Glińki", 60),
+           new Pracownik("Regina", "Turska", 60),
+           new Pracownik("Mikołaj", "Żukowski",100),
+           new Pracownik("Krzysztof", "Glińki", 50),
         });
-        System.out.printf("3 day cost: %.2f%n", firma.ObliczKoszt(Czas.DAY, 3));
-        System.out.printf("Daily cost: %.2f%n", firma.obliczKoszt(Czas.DAY));
-        System.out.printf("Monthly cost: %.2f%n", firma.obliczMiesiecznyKosztFirmy());
-        System.out.printf("Yearly cost: %.2f%n", firma.obliczRocznyKosztFirmy());
+        System.out.printf("3dzienny Koszt: %.2f%n", firma.obliczKoszt(Time.DAY, 3));
+        System.out.printf("Dzienny Koszt: %.2f%n", firma.obliczKoszt(Time.DAY));
+        System.out.printf("Miesieczny Koszt: %.2f%n", firma.obliczMiesiecznyKosztFirmy());
+        System.out.printf("Roczny Koszt: %.2f%n", firma.obliczRocznyKosztFirmy());
     }
     
 }
